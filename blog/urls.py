096b987adb44f23cacf6fe6e7866bb7432b2b0a9
xuqh <865253988@qq.com>
2016-12-06 20:51:37 +0800

    change url order

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns=[
-    url(r'^$',views.posts_list,name='posts_list'),
-    url(r'^post/(?P<pk>\d+)/$',views.posts_detail,name='posts_detail'),
     url(r'^heatmap$',views.heatmap,name='heatmap'),
     url(r'^usermap$',views.usermap,name='usermap'),
     url(r'^current_user$',views.current_user,name='current_user'),
+    url(r'^$',views.posts_list,name='posts_list'),
+    url(r'^post/(?P<pk>\d+)/$',views.posts_detail,name='posts_detail'),
+
     #url(r'^usermap',views.posts_list,name='posts_list'),
 ]
