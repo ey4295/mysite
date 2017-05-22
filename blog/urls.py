80c608f43d33ae011669c2e6673309a81d6bef10
xuqh <865253988@qq.com>
2017-05-23 00:17:52 +0800

    basic outline for named entity recognition

--- blog/urls.py
+++ blog/urls.py
@@ -1,21 +1,19 @@
-import django
 from django.conf.urls import url, include
-
 
 from blog import views
 
-urlpatterns=[
-    url(r'^heatmap$',views.heatmap,name='heatmap'),
-    url(r'^usermap$',views.usermap,name='usermap'),
-    url(r'^current_user$',views.current_user,name='current_user'),
-    url(r'^$',views.posts_list,name='posts_list'),
-    url(r'^post/(?P<pk>\d+)/$',views.posts_detail,name='posts_detail'),
-    url(r'^post/write$',views.writePost,name='writePost'),
+urlpatterns = [
+    url(r'^heatmap$', views.heatmap, name='heatmap'),
+    url(r'^usermap$', views.usermap, name='usermap'),
+    url(r'^current_user$', views.current_user, name='current_user'),
+    url(r'^$', views.posts_list, name='posts_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.posts_detail, name='posts_detail'),
+    url(r'^post/write$', views.writePost, name='writePost'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.editPost, name='editPost'),
-    url(r'^sendmsg$',views.sendmsg,name='sendmsg'),
-    url(r'^success$',views.success,name='success'),
-    url(r'^failure$',views.failure,name='failure'),
+    url(r'^sendmsg$', views.sendmsg, name='sendmsg'),
+    url(r'^success$', views.success, name='success'),
+    url(r'^failure$', views.failure, name='failure'),
+    url(r'^ner$', views.ner, name='ner'),
     url(r'^froala_editor/', include('froala_editor.urls')),
-
-    #url(r'^usermap',views.posts_list,name='posts_list'),
+    # url(r'^usermap',views.posts_list,name='posts_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,8 @@
-from time import timezone
-
 from django.contrib.auth.decorators import login_required
-from django.forms import CharField
 from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
-from froala_editor.widgets import FroalaEditor
-
 from blog.forms import PostForm, SendMessage
-from blog.models import Post, User_Request, Message
+from blog.models import Post, User_Request
 from blog.tools.send_messages import send_text
 
 
@@ -18,31 +12,37 @@ def register_visitor(request):
     req.REMOTE_ADDR = request.META["REMOTE_ADDR"]
     req.store()
 
+
 def posts_list(request):
     register_visitor(request)
-    posts=Post.objects.all().order_by("-created_date")
-    return render(request,'blog/posts_list.html',{'posts':posts,})
+    posts = Post.objects.all().order_by("-created_date")
+    return render(request, 'blog/posts_list.html', {'posts': posts, })
 
-def posts_detail(request,pk):
+
+def posts_detail(request, pk):
     register_visitor(request)
-    post=Post.objects.get(pk=pk)
-    return render(request,'blog/posts_detail.html',{'post':post,})
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/posts_detail.html', {'post': post, })
+
 
 def heatmap(request):
     register_visitor(request)
-    return render(request,'blog/Heatmap.html',{})
+    return render(request, 'blog/Heatmap.html', {})
+
 
 def usermap(request):
     register_visitor(request)
-    return render(request,'blog/Usermap.html',{})
+    return render(request, 'blog/Usermap.html', {})
+
 
 def current_user(request):
-    requests=User_Request.objects.all()
-    IPs=[req.REMOTE_ADDR for req in requests ]
-    IPs={}.fromkeys(IPs).keys()
-    total=len(IPs)
+    requests = User_Request.objects.all()
+    IPs = [req.REMOTE_ADDR for req in requests]
+    IPs = {}.fromkeys(IPs).keys()
+    total = len(IPs)
     register_visitor(request)
-    return render(request,'blog/Current_user.html',{'total':total,})
+    return render(request, 'blog/Current_user.html', {'total': total, })
+
 
 @login_required
 def writePost(request):
@@ -51,21 +51,22 @@ def writePost(request):
     :param request:
     :return:a template with form
     """
-    if request.method=="POST":
-        form=PostForm(request.POST)
+    if request.method == "POST":
+        form = PostForm(request.POST)
         if form.is_valid():
-            post=form.save(commit=False)
+            post = form.save(commit=False)
             post.publish()
-            return redirect('posts_detail',pk=post.pk)
+            return redirect('posts_detail', pk=post.pk)
     else:
         register_visitor(request)
-        #form=CharField(widget=FroalaEditor)
-        form=PostForm()
-        #form=FroalaEditor()
-    return render(request, 'blog/writePost.html', {'form':form})#FIXIT
+        # form=CharField(widget=FroalaEditor)
+        form = PostForm()
+        # form=FroalaEditor()
+    return render(request, 'blog/writePost.html', {'form': form})  # FIXIT
+
 
 @login_required
-def editPost(request,pk):
+def editPost(request, pk):
     """
     get original post &&& save edit
     :param request:Http request send by user
@@ -82,6 +83,7 @@ def editPost(request,pk):
         form = PostForm(instance=post)
     return render(request, 'blog/editPost.html', {'form': form})
 
+
 @login_required()
 def sendmsg(request):
     """
@@ -89,26 +91,26 @@ def sendmsg(request):
     :param request: post or get
     :return: void
     """
-    if request.method=="POST":
-        form=SendMessage(request.POST)
+    if request.method == "POST":
+        form = SendMessage(request.POST)
         if form.is_valid():
-            msg=form.save(commit=False)
-            to_cell="+86{0}".format(msg.to_cell)
-            content=msg.content
+            msg = form.save(commit=False)
+            to_cell = "+86{0}".format(msg.to_cell)
+            content = msg.content
             msg.store()
             try:
                 print (to_cell)
 
-                send_text(to_cell=str(to_cell),message=str(content))
+                send_text(to_cell=str(to_cell), message=str(content))
 
                 return redirect('success')
             except Exception as err:
-                request.session['st']=str(err)
+                request.session['st'] = str(err)
                 return redirect('failure')
 
     else:
-        form=SendMessage()
-        return render(request,'blog/send_msg.html',{'form':form})
+        form = SendMessage()
+        return render(request, 'blog/send_msg.html', {'form': form})
 
 
 def failure(request):
@@ -117,7 +119,8 @@ def failure(request):
     :param request:
     :return: void
     """
-    return render(request, 'blog/failure.html',{'error':request.session.get('st')})
+    return render(request, 'blog/failure.html', {'error': request.session.get('st')})
+
 
 def success(request):
     """
@@ -126,3 +129,12 @@ def success(request):
     :return: void
     """
     return render(request, 'blog/success.html')
+
+
+def ner(request):
+    """
+    Named Entity Recognition Page
+    :param request: web request
+    :return: template
+    """
+    return render(request, 'blog/ner.html')
